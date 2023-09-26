5d4b64e78348c9464f3662877b993cd6d2be6e87
cogi234 <colinbougie@gmail.com>
2023-09-26 08:11:29 -0400

    Starting stuff

---
+++ Assets/Scripts/GameGraphs.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathfindingLib;
+
+public class GameGraphs : MonoBehaviour
+{
+    public enum AdjacencyType { List, Matrix, ListCosts, MatrixCosts };
+
+    [SerializeField] List<Vector3> vertexPositions;
+    [SerializeField] AdjacencyType adjacencyType;
+
+    AdjacencyList adjacencyList;
+    AdjacencyMatrix adjacencyMatrix;
+    AdjacencyListCosts adjacencyListCosts;
+    AdjacencyMatrixCosts adjacencyMatrixCosts;
+
+    private void Awake()
+    {
+        adjacencyList = new AdjacencyList(vertexPositions.Count);
+        adjacencyMatrix = new AdjacencyMatrix(vertexPositions.Count);
+        adjacencyListCosts = new AdjacencyListCosts(vertexPositions.Count);
+        adjacencyMatrixCosts = new AdjacencyMatrixCosts(vertexPositions.Count);
+    }
+}
