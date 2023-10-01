a8a27e45c1bdcba8a34a7fd49ccf77c0e8ce115c
ancario <anthonylabbe68@gmail.com>
2023-10-01 14:10:10 -0400

    c
    c

--- Assets/Scripts/william/GameGrap2.cs
+++ Assets/Scripts/william/GameGrap2.cs
@@ -1,6 +1,7 @@
 using PathfindingLib;
 using System.Collections.Generic;
 using System.Drawing;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -15,32 +16,41 @@ public class GameGrap2 : MonoBehaviour
     public List<Graph> graphs;
     List<IGraphRepresentation> graphsRepresentation = new List<IGraphRepresentation>();
 
+    private void Start()
+    {
+        trouverChemin();
+    }
+
+
 
     private void OnDrawGizmos()
     {
-        Vector2Int startPostion = new Vector2Int();
-        for (int i = 0; i < graphs.Count; ++i)
-        {
-            switch (graphs[i].type)
+       
+            Vector2Int startPostion = new Vector2Int();
+            for (int i = 0; i < graphs.Count; ++i)
             {
-                case AdjacencyType.List:
-                    graphsRepresentation.Insert(i, new AdjacencyMatrix(graphs[i].size.x * graphs[i].size.y));
-                    break;
-                case AdjacencyType.Matrix:
-                    graphsRepresentation.Insert(i, new AdjacencyMatrixCosts(graphs[i].size.x * graphs[i].size.y));
-                    break;
-                case AdjacencyType.ListCosts:
-                    graphsRepresentation.Insert(i, new AdjacencyList(graphs[i].size.x * graphs[i].size.y));
-                    break;
-                case AdjacencyType.MatrixCosts:
-                    graphsRepresentation.Insert(i, new AdjacencyListCosts(graphs[i].size.x * graphs[i].size.y));
-                    break;
+                switch (graphs[i].type)
+                {
+                    case AdjacencyType.List:
+                        graphsRepresentation.Insert(i, new AdjacencyMatrix(graphs[i].size.x * graphs[i].size.y));
+                        break;
+                    case AdjacencyType.Matrix:
+                        graphsRepresentation.Insert(i, new AdjacencyMatrixCosts(graphs[i].size.x * graphs[i].size.y));
+                        break;
+                    case AdjacencyType.ListCosts:
+                        graphsRepresentation.Insert(i, new AdjacencyList(graphs[i].size.x * graphs[i].size.y));
+                        break;
+                    case AdjacencyType.MatrixCosts:
+                        graphsRepresentation.Insert(i, new AdjacencyListCosts(graphs[i].size.x * graphs[i].size.y));
+                        break;
+                }
+                CreateNodeMap2D(startPostion, new Vector2Int(graphs[i].size.x, graphs[i].size.y), graphs[i].emptyNodes, graphsRepresentation[i],
+                    graphs[i].start, graphs[i].end);
+                startPostion += new Vector2Int(graphs[i].size.x + distanceBetweenGraph, 0);
             }
-            CreateNodeMap2D(startPostion, new Vector2Int(graphs[i].size.x, graphs[i].size.y), graphs[i].emptyNodes, graphsRepresentation[i],
-                graphs[i].start, graphs[i].end);
-            startPostion += new Vector2Int(graphs[i].size.x + distanceBetweenGraph, 0);
-        }
-
+          
+        
+       
     }
     public void AddEdge(int a, int b, IGraphRepresentation graph)
     {
@@ -96,6 +106,30 @@ public class GameGrap2 : MonoBehaviour
                     }
                 }
             }
+        }
+        trouverChemin();
+    }
+    private int Distance(int p1, int p2)
+    {
+        return (int)Vector2.Distance(nodes[p1].position, nodes[p2].position);
+    }
+    public void trouverChemin()
+    {
+        List<int> foundPath = Algorithms.BFS(graphsRepresentation[0], 1, 10);
+        Debug.Log(foundPath.Count);
+        foreach (int p in foundPath)
+        {
+            Debug.Log(p);
+        }
+  
+
+
+        int compteur = foundPath.Count;
+        for (int i = 0; i < compteur; i++)
+        {
+
+            Gizmos.color = UnityEngine.Color.Lerp(UnityEngine.Color.red, UnityEngine.Color.blue, 0.2f);
+            Gizmos.DrawLine(nodes[foundPath[i]].position, nodes[foundPath[i++]].position + new Vector3(1, 0, 0));
         }
     }
 }
