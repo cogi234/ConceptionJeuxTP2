6a2acaa508170748dd5d9f79f65584f961106807
cogi234 <colinbougie@gmail.com>
2023-10-05 14:28:15 -0400

    variable en anglais

--- Assets/Scripts/FollowComponent.cs
+++ Assets/Scripts/FollowComponent.cs
@@ -2,14 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR;
 
 public class FollowComponent : MonoBehaviour
 {
     [SerializeField] float speed = 5;
     [SerializeField] GameGraphs graph;
     List<int> path;
-    int compteurPath = 0;
+    int pathCounter = 0;
 
     private void Start()
     {
@@ -22,13 +21,13 @@ public class FollowComponent : MonoBehaviour
     void Update()
     {
         //If we have a path and we aren't at the end
-        if (path != null && compteurPath < path.Count) 
+        if (path != null && pathCounter < path.Count) 
         {
-            transform.position = Vector3.MoveTowards(transform.position, graph.GetPosition(path[compteurPath]), speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, graph.GetPosition(path[pathCounter]), speed * Time.deltaTime);
             //If we reached the point, we switch to the next
-            if (Vector3.Distance(transform.position, graph.GetPosition(path[compteurPath])) < 0.1f)
+            if (Vector3.Distance(transform.position, graph.GetPosition(path[pathCounter])) < 0.1f)
             {
-                    compteurPath++;
+                    pathCounter++;
             }
         }
     }
@@ -38,7 +37,7 @@ public class FollowComponent : MonoBehaviour
         graph.FindPath();
         path = graph.path;
 
-        compteurPath = 0;
+        pathCounter = 0;
         transform.position = graph.GetPosition(path[0]);
     }
 }
